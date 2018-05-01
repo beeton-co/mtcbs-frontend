406a439592853f89f00a114f70e734c2de3277ba
Katika <katika@beeton.co>
2018-05-01 10:32:12 -0400

    add empty race notification view

---
+++ src/components/Race/EmptyRaceView/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Alert} from 'antd';
+
+
+export const EmptyRaceView = (props) => {
+  const {description, message, type} = props;
+
+  return (
+          <div style={{ marginTop: 0 }} >
+            <Alert
+                    message={message}
+                    description={description}
+                    type={type}
+                    showIcon
+            />
+          </div>
+  );
+};
