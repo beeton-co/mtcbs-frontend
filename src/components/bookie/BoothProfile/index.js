ed393acbc342aadaf8d7b768162ec6e369004e31
Katika <katika@beeton.co>
2018-05-30 08:14:12 -0400

    account loading

--- src/components/bookie/BoothProfile/index.js
+++ src/components/bookie/BoothProfile/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import * as utils from "../../../actions/utils";
-import {Card, Row, Col} from 'antd';
+import {Card, Row, Col, Button} from 'antd';
 import {Redirect} from 'react-router-dom'
 import DescriptionList from '../../../components/DescriptionList';
 
@@ -8,8 +8,8 @@ const {Description} = DescriptionList;
 
 const ProfileEntry = (props) => {
   const {name, value} = props;
-  return(<Row gutter={40} style={{marginBottom: 4}}>
-    <Col sm={2} className='description' >
+  return (<Row gutter={40} style={{marginBottom: 4}}>
+    <Col sm={2} className='description'>
       <div className='term'>{name}</div>
     </Col>
     <Col sm={10} className='description'>
@@ -19,24 +19,36 @@ const ProfileEntry = (props) => {
     </Col>
   </Row>);
 };
+const AccountEntry = (props) => {
+  const {name, value, handleLoadAccount} = props;
+  return (<Row gutter={40} style={{marginBottom: 4}}>
+    <Col sm={2} className='description'>
+      <div className='term'>{name}</div>
+    </Col>
+    <Col sm={10} className='description'>
 
-const AccountEntry = () => {
-  /*
-  <Button type="primary" htmlType="submit" loading={submitting} disabled={pristine || submitting}>
-          Create
-        </Button>
-   */
+      <Row>
+        <Col sm={2}>
+          <div className='detail'>{value}</div>
+        </Col>
+        <Col sm={5}><Button type="primary" onClick={handleLoadAccount}>Load Account</Button></Col>
+        <Col sm={3}></Col>
+      </Row>
+    </Col>
+    <Col sm={10}>
+    </Col>
+  </Row>);
 };
 
 export default class BoothProfile extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.handleLoadAccount = this.handleLoadAccount.bind(this);
   }
 
-  handleLoadAccount(){
-
+  handleLoadAccount() {
   }
+
   render() {
     if (utils.isNull(this.props.econtract.user) || !this.props.econtract.user.channelOwner) {
       return <Redirect to='/' {...this.props} />;
@@ -47,14 +59,14 @@ export default class BoothProfile extends Component {
     const name = {'name': 'Name', 'value': contract.user.channel.name};
     const description = {'name': 'Description', 'value': contract.user.channel.description};
     const accountValue = contract.user.channel.account + ' ETH';
-    const account = {'name': 'Account Balance', 'value': accountValue};
+    const account = {'name': 'Account Balance', 'value': accountValue, handleLoadAccount: this.handleLoadAccount};
     const network = {'name': 'Network', 'value': contract.network.id};
 
     return (<Card bordered={false} title="Channel Profile">
-      <div className='descriptionList' style={{marginBottom: 32}} size="large" >
+      <div className='descriptionList' style={{marginBottom: 32}} size="large">
         <ProfileEntry {...name}/>
         <ProfileEntry {...description}/>
-        <ProfileEntry {...account}/>
+        <AccountEntry {...account}/>
         <ProfileEntry {...network}/>
       </div>
     </Card>);
